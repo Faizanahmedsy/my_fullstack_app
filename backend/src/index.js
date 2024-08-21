aaef958d7d19238e89cdb7d97124d4ce23e0815e
Faizanahmedsy <saiyed.faizanahmed1@gmail.com>
2024-08-21 23:19:16 +0530

    connected mongodb

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,25 +1,31 @@
+import dotenv from "dotenv";
 import express from "express";
-import cors from "cors";
+import connectDb from "./db/index.js";
+// import cors from "cros";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDb();
 const app = express();
 
-const PORT = 8000;
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//   })
+// );
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-  })
-);
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// app.get("/api/express", (req, res) => {
+//   res.json({
+//     message: "Hello from express backend",
+//   });
+// });
 
-app.get("/api/express", (req, res) => {
-  res.json({
-    message: "Hello from express backend",
-  });
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
