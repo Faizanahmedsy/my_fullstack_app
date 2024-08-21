c738870e69d5b60b885082f04b1342d0cb04c35f
Faizanahmedsy <saiyed.faizanahmed1@gmail.com>
2024-08-21 23:30:08 +0530

    refactor

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,23 @@
 import dotenv from "dotenv";
 import express from "express";
 import connectDb from "./db/index.js";
+import app from "./app.js";
 // import cors from "cros";
+const app = express();
 
 dotenv.config({
   path: "./env",
 });
 
-connectDb();
-const app = express();
+connectDb()
+  .then(() => {
+    app.on("error", (err) => console.log("error", err));
+
+    app.listen(process.env.PORT || 8080, () => {
+      console.log(`Server is running on port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.log("error in monogo connection", err));
 
 // app.use(
 //   cors({
